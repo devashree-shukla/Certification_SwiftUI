e08315b1f862706ef65ff7a0ee464105c76514fd
Devashree Shukla <shukladevashree963@gmail.com>
2024-05-18 16:52:54 +0530

    Basic app setup

---
+++ PizzaApp/PizzaApp/Main/PizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppApp.swift
+//  PizzaApp
+//
+//  Created by devashree shukla on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PizzaApp/PizzaApp/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PizzaApp
+//
+//  Created by devashree shukla on 18/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
